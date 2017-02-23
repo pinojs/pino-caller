be4886709200eb08e57fd6cee9353c6d4c4ed2d7
phra <greensoncio@gmail.com>
2017-02-23 21:17:20 +0000

    align to standard linting conventions

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-"use strict";
+'use strict'
 
 const OFFSET = process && process.version[1] > 6 ? 2 : 3
 
-function _makeCallSiteGetter(pinoInstance, level) {
-    return function () {
-        var child = pinoInstance.child({
-            caller: Error().stack.split('\n')[OFFSET].substr(7)
-        })
-
-        return child[level].apply(child, arguments)
-    }
-}
-
-function traceCaller(pinoInstance) {
-    var facade = Object.create(pinoInstance)
-    Object.keys(pinoInstance.levels.values).forEach(function (level) {
-        facade[level] = _makeCallSiteGetter(pinoInstance, level)
+function _makeCallSiteGetter (pinoInstance, level) {
+  return function () {
+    const child = pinoInstance.child({
+      caller: Error().stack.split('\n')[OFFSET].substr(7)
     })
 
-    return facade
+    return child[level].apply(child, arguments)
+  }
+}
+
+function traceCaller (pinoInstance) {
+  const facade = Object.create(pinoInstance)
+  Object.keys(pinoInstance.levels.values).forEach(function (level) {
+    facade[level] = _makeCallSiteGetter(pinoInstance, level)
+  })
+
+  return facade
 }
 
 module.exports = traceCaller
--- tests/test.js
+++ tests/test.js
@@ -1,20 +1,20 @@
-"use strict";
+'use strict'
 
-var test = require('tap').test;
-var pino = require('pino');
-var through2 = require('through2');
-var pinoCaller = require("../");
+var test = require('tap').test
+var pino = require('pino')
+var through2 = require('through2')
+var pinoCaller = require('../')
 
 test('pino caller works', function (t) {
-    t.plan(3)
+  t.plan(3)
 
-    const pinoInstance = pinoCaller(pino(through2(function (chunk, enc, callback) {
-        const res = JSON.parse(chunk.toString('utf8'))
-        const regex = /Test.<anonymous> \(\/(.)*tests\/test.js/
-        t.ok(res.caller, 'caller property is set')
-        t.equal(typeof res.caller, 'string', 'caller property is a string')
-        t.ok(regex.test(res.caller), 'caller property matches the test regex')
-    })))
+  const pinoInstance = pinoCaller(pino(through2(function (chunk, enc, callback) {
+    const res = JSON.parse(chunk.toString('utf8'))
+    const regex = /Test.<anonymous> \(\/(.)*tests\/test.js/
+    t.ok(res.caller, 'caller property is set')
+    t.equal(typeof res.caller, 'string', 'caller property is a string')
+    t.ok(regex.test(res.caller), 'caller property matches the test regex')
+  })))
 
-    pinoInstance.info('test')
+  pinoInstance.info('test')
 })
