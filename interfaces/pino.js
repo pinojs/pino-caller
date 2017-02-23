23efc3b82370e1b2123920b0f9de28b238e6d29c
phra <greensoncio@gmail.com>
2017-02-23 16:14:42 +0000

    initial version

--- interfaces/pino.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicGluby5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbInBpbm8udHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiJ9
--- tests/test.js
+++ tests/test.js
@@ -1,14 +1,20 @@
 "use strict";
-var tape = require("tape");
-var pino = require("pino");
-var through2 = require("through2");
-var _1 = require("../");
-tape('extending works', function (tape) {
-    tape.plan(1);
-    var pinoInstance = _1.default(pino(through2(function (chunk, enc, callback) {
-        console.log(chunk.toString('utf8'));
-        tape.true(true);
-    })));
-    pinoInstance.info('test');
-});
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidGVzdC5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbInRlc3QudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IjtBQUFBLDJCQUE0QjtBQUM1QiwyQkFBNEI7QUFDNUIsbUNBQW9DO0FBQ3BDLHdCQUE0QztBQU81QyxJQUFJLENBQUMsaUJBQWlCLEVBQUUsVUFBQSxJQUFJO0lBQ3hCLElBQUksQ0FBQyxJQUFJLENBQUMsQ0FBQyxDQUFDLENBQUE7SUFDWixJQUFNLFlBQVksR0FBRyxVQUFXLENBQUMsSUFBSSxDQUFDLFFBQVEsQ0FBQyxVQUFDLEtBQUssRUFBRSxHQUFHLEVBQUUsUUFBUTtRQUNoRSxPQUFPLENBQUMsR0FBRyxDQUFDLEtBQUssQ0FBQyxRQUFRLENBQUMsTUFBTSxDQUFDLENBQUMsQ0FBQztRQUNwQyxJQUFJLENBQUMsSUFBSSxDQUFDLElBQUksQ0FBQyxDQUFBO0lBQ25CLENBQUMsQ0FBQyxDQUFDLENBQUMsQ0FBQTtJQUVKLFlBQVksQ0FBQyxJQUFJLENBQUMsTUFBTSxDQUFDLENBQUM7QUFDOUIsQ0FBQyxDQUFDLENBQUEifQ==
+
+var test = require('tap').test;
+var pino = require('pino');
+var through2 = require('through2');
+var pinoCaller = require("../");
+
+test('pino caller works', function (t) {
+    t.plan(3)
+
+    const pinoInstance = pinoCaller(pino(through2(function (chunk, enc, callback) {
+        const res = JSON.parse(chunk.toString('utf8'))
+        const regex = /Test.<anonymous> \(\/(.)*tests\/test.js/
+        t.ok(res.caller, 'caller property is set')
+        t.equal(typeof res.caller, 'string', 'caller property is a string')
+        t.ok(regex.test(res.caller), 'caller property matches the test regex')
+    })))
+
+    pinoInstance.info('test')
+})
