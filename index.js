5a11aec6a45af2b3d5be40b16f45eb5c359a98a6
phra <greensoncio@gmail.com>
2017-03-01 20:13:00 +0000

    fix wrapper when changing log level at runtime

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 'use strict'
 
-const OFFSET = process && process.version[1] > 6 ? 2 : 3
+const STACKTRACE_OFFSET = process && process.version[1] > 6 ? 2 : 3
+const LINE_OFFSET = 7
 
 function _makeCallSiteGetter (pinoInstance, level) {
   return function () {
     const child = pinoInstance.child({
-      caller: Error().stack.split('\n')[OFFSET].substr(7)
+      caller: Error().stack.split('\n')[STACKTRACE_OFFSET].substr(LINE_OFFSET)
     })
 
     return child[level].apply(child, arguments)
@@ -15,7 +16,7 @@ function _makeCallSiteGetter (pinoInstance, level) {
 function traceCaller (pinoInstance) {
   const facade = Object.create(pinoInstance)
   Object.keys(pinoInstance.levels.values).forEach(function (level) {
-    facade[level] = _makeCallSiteGetter(pinoInstance, level)
+    facade[level] = facade[`_${level}`] = _makeCallSiteGetter(pinoInstance, level)
   })
 
   return facade
