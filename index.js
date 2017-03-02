a8b04fa0c8d2c4e194a8b77b0f3089a58652ad23
phra <greensoncio@gmail.com>
2017-03-02 22:02:34 +0000

    switch way of monkey patching pino instance

--- index.js
+++ index.js
@@ -1,25 +1,16 @@
 'use strict'
 
-const STACKTRACE_OFFSET = process && process.version[1] > 6 ? 2 : 3
+const STACKTRACE_OFFSET = process && process.version[1] > 6 ? 4 : 5
 const LINE_OFFSET = 7
 
-function _makeCallSiteGetter (pinoInstance, level) {
-  return function () {
-    const child = pinoInstance.child({
-      caller: Error().stack.split('\n')[STACKTRACE_OFFSET].substr(LINE_OFFSET)
-    })
-
-    return child[level].apply(child, arguments)
-  }
-}
-
 function traceCaller (pinoInstance) {
-  const facade = Object.create(pinoInstance)
-  Object.keys(pinoInstance.levels.values).forEach(function (level) {
-    facade[level] = facade[`_${level}`] = _makeCallSiteGetter(pinoInstance, level)
+  return Object.assign(Object.create(pinoInstance), {
+    asJson: function (...args) {
+      args[0] = args[0] || Object.create(null)
+      args[0].caller = Error().stack.split('\n')[STACKTRACE_OFFSET].substr(LINE_OFFSET)
+      return pinoInstance.asJson.apply(this, args)
+    }
   })
-
-  return facade
 }
 
 module.exports = traceCaller
--- tests/test.js
+++ tests/test.js
@@ -18,3 +18,40 @@ test('pino caller works', function (t) {
 
   pinoInstance.info('test')
 })
+
+test('pino caller works also when adding level', function (t) {
+  t.plan(3)
+
+  const pinoInstance = pinoCaller(pino(through2(function (chunk, enc, callback) {
+    const res = JSON.parse(chunk.toString('utf8'))
+    const regex = /Test.<anonymous> \(\/(.)*tests\/test.js/
+    t.ok(res.caller, 'caller property is set')
+    t.equal(typeof res.caller, 'string', 'caller property is a string')
+    t.ok(regex.test(res.caller), 'caller property matches the test regex')
+  })))
+
+  pinoInstance.level = 'fatal'
+  pinoInstance.level = 'trace'
+  pinoInstance.level = 'fatal'
+  pinoInstance.trace('test')
+  pinoInstance.fatal('test')
+})
+
+test('pino caller works also when switching level', function (t) {
+  t.plan(3)
+
+  const pinoInstance = pinoCaller(pino(through2(function (chunk, enc, callback) {
+    const res = JSON.parse(chunk.toString('utf8'))
+    const regex = /Test.<anonymous> \(\/(.)*tests\/test.js/
+    t.ok(res.caller, 'caller property is set')
+    t.equal(typeof res.caller, 'string', 'caller property is a string')
+    t.ok(regex.test(res.caller), 'caller property matches the test regex')
+  })))
+
+  pinoInstance.addLevel('myLevel', 100)
+  pinoInstance.level = 'myLevel'
+  pinoInstance.level = 'trace'
+  pinoInstance.level = 'myLevel'
+  pinoInstance.trace('test')
+  pinoInstance.myLevel('test')
+})
