f9564ee252661448afd7007ce568cc109ae79c6d
phra <greensoncio@gmail.com>
2017-03-03 14:27:22 +0000

    fallback from rest parameters to arguments slice trick

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const LINE_OFFSET = 7
 
 function traceCaller (pinoInstance) {
   return Object.assign(Object.create(pinoInstance), {
-    asJson: function (...args) {
+    asJson: function () {
+      const args = Array.prototype.slice.call(arguments)
       args[0] = args[0] || Object.create(null)
       args[0].caller = Error().stack.split('\n')[STACKTRACE_OFFSET].substr(LINE_OFFSET)
       return pinoInstance.asJson.apply(this, args)
