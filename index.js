6406f2bb708fff79b4ecf105dd694a267986babc
phra <greensoncio@gmail.com>
2017-05-25 17:01:08 +0100

    bypass asJson as not configurable/writable via es6 proxy, fixes #2

--- index.js
+++ index.js
@@ -4,14 +4,18 @@ const STACKTRACE_OFFSET = process && process.version[1] > 6 ? 4 : 5
 const LINE_OFFSET = 7
 
 function traceCaller (pinoInstance) {
-  return Object.assign(Object.create(pinoInstance), {
-    asJson: function () {
-      const args = Array.prototype.slice.call(arguments)
-      args[0] = args[0] || Object.create(null)
-      args[0].caller = Error().stack.split('\n')[STACKTRACE_OFFSET].substr(LINE_OFFSET)
-      return pinoInstance.asJson.apply(this, args)
-    }
-  })
+  function get (target, name) {
+    return name === 'asJson' ? asJson : target[name]
+  }
+
+  function asJson () {
+    const args = Array.prototype.slice.call(arguments)
+    args[0] = args[0] || Object.create(null)
+    args[0].caller = Error().stack.split('\n')[STACKTRACE_OFFSET].substr(LINE_OFFSET)
+    return pinoInstance.asJson.apply(this, args)
+  }
+
+  return new Proxy(pinoInstance, { get: get })
 }
 
 module.exports = traceCaller
