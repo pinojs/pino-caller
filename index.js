5e8d238993b78254b067ae3fe0b08b6e5eea862f
gregory <greg2502@gmail.com>
2019-11-20 13:02:52 -0800

    [fixes #16] allow to pass dynamic offsets

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 'use strict'
 
 const NODEJS_VERSION = parseInt(process.version.slice(1).split('.')[0], 10)
-const STACKTRACE_OFFSET = NODEJS_VERSION && NODEJS_VERSION > 6 ? 4 : 5
-const LINE_OFFSET = 7
+const DEFAULT_STACKTRACE_OFFSET = NODEJS_VERSION && NODEJS_VERSION > 6 ? 4 : 5
+const DEFAULT_LINE_OFFSET = 7
 const { symbols } = require('pino')
 const { asJsonSym } = symbols
 
-function traceCaller (pinoInstance) {
+function traceCaller (pinoInstance, {stackTraceOffset = 0, lineOffset = 0} = {}) {
   function get (target, name) {
     return name === asJsonSym ? asJson : target[name]
   }
 
   function asJson (...args) {
     args[0] = args[0] || Object.create(null)
-    args[0].caller = Error().stack.split('\n')[STACKTRACE_OFFSET].substr(LINE_OFFSET)
+    args[0].caller = Error().stack.split('\n')[stackTraceOffset + DEFAULT_STACKTRACE_OFFSET].substr(lineOffset + DEFAULT_LINE_OFFSET)
     return pinoInstance[asJsonSym].apply(this, args)
   }
 
--- tests/test.js
+++ tests/test.js
@@ -5,6 +5,23 @@ var pino = require('pino')
 var through2 = require('through2')
 var pinoCaller = require('../')
 
+test('pino caller works when passing custom stacktrace  Offset', function (t) {
+  t.plan(3)
+  const offsets = {
+    stackTraceOffset: 2,
+    lineOffset: 1
+  }
+
+  const pinoInstance = pinoCaller(pino(through2(function (chunk, enc, callback) {
+    const res = JSON.parse(chunk.toString('utf8'))
+    const regex = /est\.runBound \(domain\.js:\d*:\d*\)/
+    t.ok(res.caller, 'caller property is set')
+    t.equal(typeof res.caller, 'string', 'caller property is a string')
+    t.ok(regex.test(res.caller), 'caller property matches the test regex')
+  })), offsets)
+
+  pinoInstance.info('test')
+})
 test('pino caller works', function (t) {
   t.plan(3)
 
