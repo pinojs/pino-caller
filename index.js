cfbf909e190025ccfde26de4e92ba6ed9fba4a7e
gregory <greg2502@gmail.com>
2019-11-21 14:56:12 -0800

    update default offset & ignore pino module

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const NODEJS_VERSION = parseInt(process.version.slice(1).split('.')[0], 10)
-const DEFAULT_STACKTRACE_OFFSET = NODEJS_VERSION && NODEJS_VERSION > 6 ? 4 : 5
+const DEFAULT_STACKTRACE_OFFSET = NODEJS_VERSION && NODEJS_VERSION > 6 ? 2 : 3
 const DEFAULT_LINE_OFFSET = 7
 const { symbols } = require('pino')
 const { asJsonSym } = symbols
@@ -13,7 +13,8 @@ function traceCaller (pinoInstance, {stackTraceOffset = 0, lineOffset = 0} = {})
 
   function asJson (...args) {
     args[0] = args[0] || Object.create(null)
-    args[0].caller = Error().stack.split('\n')[stackTraceOffset + DEFAULT_STACKTRACE_OFFSET].substr(lineOffset + DEFAULT_LINE_OFFSET)
+    args[0].caller = Error().stack.split('\n')
+    .filter(s => !s.includes('node_modules/pino'))[stackTraceOffset + DEFAULT_STACKTRACE_OFFSET].substr(lineOffset + DEFAULT_LINE_OFFSET)
     return pinoInstance[asJsonSym].apply(this, args)
   }
 
