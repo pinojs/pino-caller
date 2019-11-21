7ca7ea2e1281b7f0a9944d63db45bd24c9c6530b
phra <greensoncio@gmail.com>
2019-11-21 23:33:40 +0100

    fix: ignore pino and plugins in stacktrace

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const NODEJS_VERSION = parseInt(process.version.slice(1).split('.')[0], 10)
-const STACKTRACE_OFFSET = NODEJS_VERSION && NODEJS_VERSION > 6 ? 4 : 5
+const STACKTRACE_OFFSET = NODEJS_VERSION && NODEJS_VERSION > 6 ? 2 : 3
 const LINE_OFFSET = 7
 const { symbols } = require('pino')
 const { asJsonSym } = symbols
@@ -13,7 +13,7 @@ function traceCaller (pinoInstance) {
 
   function asJson (...args) {
     args[0] = args[0] || Object.create(null)
-    args[0].caller = Error().stack.split('\n')[STACKTRACE_OFFSET].substr(LINE_OFFSET)
+    args[0].caller = Error().stack.split('\n').filter(s => !s.includes('node_modules/pino'))[STACKTRACE_OFFSET].substr(LINE_OFFSET)
     return pinoInstance[asJsonSym].apply(this, args)
   }
 
