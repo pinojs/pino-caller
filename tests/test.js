ac44483fa82ee2750b8440213275769fcc65b43c
Francesco Soncina <phra@users.noreply.github.com>
2019-11-23 08:47:03 +0100

    Ignore pino and plugins in stacktrace (#18)
    * fix: ignore pino and plugins in stacktrace
    
    * fix: support windows fs paths
    
    * test: add compat tests for plugins
    
    * deps: add pino-debug to dev deps

--- tests/test.js
+++ tests/test.js
@@ -2,6 +2,7 @@
 
 var test = require('tap').test
 var pino = require('pino')
+var pinoDebug = require('pino-debug')
 var through2 = require('through2')
 var pinoCaller = require('../')
 
@@ -56,4 +57,24 @@ test('pino caller works also when switching level', function (t) {
   pinoInstance.level = 'myLevel'
   pinoInstance.trace('test')
   pinoInstance.myLevel('test')
+})
+
+test('pino caller works also with pino plugins', function (t) {
+  t.plan(3)
+
+  const pinoInstance = pinoCaller(pino({
+    customLevels: {
+      myLevel: 100
+    }
+  }, through2(function (chunk, enc, callback) {
+    const res = JSON.parse(chunk.toString('utf8'))
+    const regex = /Test.<anonymous> \(\/(.)*tests\/test.js/
+    t.ok(res.caller, 'caller property is set')
+    t.equal(typeof res.caller, 'string', 'caller property is a string')
+    t.ok(regex.test(res.caller), 'caller property matches the test regex')
+  })))
+
+  pinoDebug(pinoInstance, { map: { '*': 'trace' } })
+
+  pinoInstance.info('test')
 })
